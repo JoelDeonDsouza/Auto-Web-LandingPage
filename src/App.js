6ca7005b3ca1eb6d4578430ccf5e96e28e677e6a
joeldeondsouza <jdeondsouza@gmail.com>
2023-06-06 02:34:33 +0200

    Added 3d img

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from "react";
 import NavigationRoutes from './Navigation/Routes';
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
 
+function Model(props) {
+  const { scene } = useGLTF("/auto2.glb");
+  return <primitive object={scene} {...props} />;
+}
 function App() {
   return (
     <div>
       <NavigationRoutes />
+      <Canvas
+      dpr={[1, 2]}
+      camera={{ fov: 45 }}
+      style={{ position: "absolute" }}
+    >
+      <color attach="background" args={["#FFFFFF"]} />
+      <PresentationControls
+        speed={1.5}
+        global
+        zoom={.5}
+        polar={[-0.1, Math.PI / 4]}
+      >
+        <Stage  environment={"sunset"}>
+          <Model scale={0.01} />
+        </Stage>
+      </PresentationControls>
+    </Canvas>
     </div>
   );
 }
